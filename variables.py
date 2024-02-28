5c6bf33bc031b765c6363adb742fe28317845a08
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-28 22:38:24 +0600

    This is how to declare variables, find data types and print them in Python.

---
+++ variables.py
@@ -0,0 +1,23 @@
+# String Variable
+name = "Irisha"
+
+# Integer Variable
+age = 13
+
+# Float Variable
+marks = 92.8
+
+# Bollean Variable
+is_student = True
+
+print(name, age, marks, is_student)
+print(type(name))
+
+# update marks value
+
+marks = 93.1
+
+print(name)
+print(age)
+print(marks)
+print(is_student)
