6f7dae78b77e653a675d3d7ac34e1727fa8ead0e
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 20:48:05 +0600

    Find the greatest number amonge three using if else condition.

---
+++ if_else_07.py
@@ -0,0 +1,11 @@
+# Find the greatest number amonge three 
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+num3 = int(input("Enter Third Number: "))
+
+if num1 > num2 and num1 > num3:
+    print(num1, "is the greatest number")
+elif num2 > num1 and num2 > num3:
+    print(num2, "is the greatest number")
+else:
+    print(num3, "is the greatest number")
