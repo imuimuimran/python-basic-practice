9acf8e7387835b2c9cb4aca44cff9700b6d2ad6f
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 14:38:56 +0600

    This program of python shows a clear concept of typecasting in python.

---
+++ type_casting_02.py
@@ -0,0 +1,23 @@
+a  = 1
+b = 2
+c = 3
+
+print(type(a))
+
+str_a = str(a)
+str_b = str(b)
+str_c = str(c)
+
+add_str = str_a + str_b + str_c
+
+print(add_str)
+
+print(type(add_str))
+
+str_to_int = int(add_str)
+
+print(type(str_to_int))
+
+d = 7
+sum = str_to_int + d
+print(sum)
