5a1b6e85bd6661ed4239c7c0c49287d91e1688d5
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 10:12:31 +0600

    This is user input program in python.

--- user_input_01.py
+++ user_input_01.py
@@ -3,5 +3,8 @@ name = input("Enter your Name: ")
 # Here input of integer acts as string
 age = input("Enter your age")
 
+roll_no = input("Enter your Roll: ")
+
 print(name)
 print(age)
+print(roll_no)
