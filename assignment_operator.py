3cce99b0fcadcbf2ca28c3cae724eb34f8421a75
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 13:07:33 +0600

    These are the basic assignment operators operations in python.

---
+++ assignment_operator.py
@@ -0,0 +1,30 @@
+'''
+Operator	Name	                    Example
+=	        Assignment Operator	        a = 7
++=	        Addition Assignment	        a += 1 # a = a + 1
+-=	        Subtraction Assignment	    a -= 3 # a = a - 3
+*=	        Multiplication Assignment	a *= 4 # a = a * 4
+/=	        Division Assignment	        a /= 3 # a = a / 3
+%=	        Remainder Assignment	    a %= 10 # a = a % 10
+**=	        Exponent Assignment	        a **= 10 # a = a ** 10
+
+'''
+
+a = 7
+print(a)
+addi = 1
+a += addi 
+print(a)
+subt = 3
+a -= subt
+print(a)
+mul = 2
+a *= mul
+print(a)
+expo = 2
+a **= expo
+print(a)
+moud = 7
+a %= moud
+print(a)
+
