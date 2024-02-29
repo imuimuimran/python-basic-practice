133a24d53e6864f191d305cf181d32af547d86b3
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 10:22:45 +0600

    This program shows how to deal with python type casting

---
+++ type_casting.py
@@ -0,0 +1,10 @@
+age01 = input("Enter your age: ")
+age02 = int(input("Enter your age: "))
+age03 = int(input("Enter your age: "))
+
+sum1 = age02 + age03
+
+print(type(age01))
+print(type(age02))
+
+print(sum1)
