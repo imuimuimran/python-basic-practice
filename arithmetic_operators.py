8023fe87cf9001c2ce86f037a0a1cfa15c205ead
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 11:54:46 +0600

    These are the basic arithmetic operations in python.

---
+++ arithmetic_operators.py
@@ -0,0 +1,20 @@
+'''
+Operator	Operation	    Example
++	        Addition	    5 + 2 = 7
+-	        Subtraction	    5 - 2 = 2
+*	        Multiplication	5 * 2 = 6
+/	        Division	    4 / 2 = 2
+//	        Floor Division	10 // 3 = 3
+%	        Modulo	        5 % 2 = 1
+**	        Power	        4 ** 2 = 16 
+
+'''
+
+print("sum of 5 + 2 = ", 5+2)
+print("difference of 5 - 2 = ", 5-2)
+print("multiplication of 5 * 2 = ", 5*2)
+print("division of 5 / 2 = ", 5/2)
+print("floor division of 5 // 2 = ", 5//2)
+print("modulo of 5 % 2 = ", 5%2)
+print("power of 5**2 = ", 5**2)
+
