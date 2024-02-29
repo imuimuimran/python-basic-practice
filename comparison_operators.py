4638eb14af92d63be9612d6c0d7ab1369bc6590d
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-02-29 13:19:21 +0600

    These are the basic comparison operators operations in python.

---
+++ comparison_operators.py
@@ -0,0 +1,20 @@
+'''
+Operator	Meaning	                    Example
+==	        Is Equal To	                3 == 5 gives us False
+!=	        Not Equal To	            3 != 5 gives us True
+>	        Greater Than	            3 > 5 gives us False
+<	        Less Than	                3 < 5 gives us True
+>=	        Greater Than or Equal To    3 >= 5 give us False
+<=	        Less Than or Equal To	    3 <= 5 gives us True
+
+'''
+
+a = 5
+b = 3
+
+print("a == b = ", a == b)
+print("a != b = ", a != b)
+print("a > b = ", a > b)
+print("a < b = ", a < b)
+print("a >= b = ", a >= b)
+print("a <= b = ", a <= b)
