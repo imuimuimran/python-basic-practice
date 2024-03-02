9f65e15d3a3f9b686d146938bf04bf802eeb48a4
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-02 09:27:37 +0600

    multiple values of list using sort and reverse functions in py

---
+++ list_08.py
@@ -0,0 +1,13 @@
+list = [12, 9.8, 100.4, 3, "Hasan", 45, "Karim", 67.2, 1]
+
+print(list)
+
+list.reverse()
+
+print(list)
+
+list2 = [12, 9.8, 100.4, 3, 45, 67.2, 1]
+
+list2.sort()
+
+print(list2)
