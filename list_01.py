9cddb1d0979ac95de21755d73b61c3b8ffd57a51
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-02 09:38:12 +0600

    basic list in python updated

--- list_01.py
+++ list_01.py
@@ -4,3 +4,10 @@ print(type(list))
 print(fruits[1])
 for fruit in fruits:
     print(fruit)
+
+print("Total item of list is:", len(fruits))
+
+if "Banan" in fruits:
+    print("Banana is in fruits")
+else:
+    print("There is no such item in the list")
