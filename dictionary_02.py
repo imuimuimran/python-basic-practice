00ad5505fdedcc59c08063ee57ffd7f231e7c617
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-03 10:37:27 +0600

    dictionary with concatinate first and last name in py

---
+++ dictionary_02.py
@@ -0,0 +1,14 @@
+person1 = {
+    "First Name" : "Mohd.",
+    "Last Name" : "Imran Hossain",
+    "Mobile" : 1234567,
+    "Marks" : 3.69,
+    "is_student" : True
+}
+
+print(person1["First Name"])
+print(person1["Last Name"])
+
+full_name = person1["First Name"] + " " + person1["Last Name"]
+
+print(full_name)
