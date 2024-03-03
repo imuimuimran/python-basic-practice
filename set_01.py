c23d8bad36e20f39f866a464bb64a71fa735aed1
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-03 14:01:26 +0600

    basic set in py

---
+++ set_01.py
@@ -0,0 +1,19 @@
+subjects = {'bangla', 'english', 'bangla', 'math'}
+
+print(subjects)
+
+subjects.add('science')
+
+print(subjects)
+
+subjects.update('geography', 'history',)
+
+print(subjects)
+
+A = {1, 2, 3, 4, 5}
+B = {3, 6, 7}
+
+
+
+print(A.union(B))
+print(A.intersection(B))
