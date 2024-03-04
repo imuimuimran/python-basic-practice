8e80ea35d73eabd7bb937fb22e58a07de7da1dde
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-04 22:36:18 +0600

    update new information in existing dictionary in py.

---
+++ dictionary_04.py
@@ -0,0 +1,17 @@
+student1 = {
+    'Name' : 'Fahim',
+    'Roll' : 1,
+    'Class' : 'IX'
+}
+
+print(student1)
+
+new_student = {
+    'Name' : 'Irisha',
+    'Roll' : 2,
+    'Class' : 'XI'
+}
+
+student1.update(new_student)
+
+print(student1)
