6faf7724c78a624991812accd30782ccdc7f5ca8
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-05 21:42:10 +0600

    print different string using same function

---
+++ function_03.py
@@ -0,0 +1,12 @@
+def print_statement(string):
+    print(string)
+    
+
+str1 = "Hello World!"
+str2= "Welcome World!"
+str3= "Welcome!"
+
+print_statement(str1)
+print_statement(str2)
+print_statement(str3)
+
