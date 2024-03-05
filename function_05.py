8818dcb9809091ca7ba453b5dc2b8b1b4ca21402
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-06 00:52:59 +0600

    example of keyword or named argument in function in py

---
+++ function_05.py
@@ -0,0 +1,10 @@
+def add_two_nums(n1, n2):
+    print("n1:", n1)
+    print("n2:", n2)
+    sum = n1 + n2
+    return sum
+
+# Keyword Argument (Named Argument)
+print(add_two_nums(n1 = 3, n2 = 2))
+
+print(add_two_nums(n1 = 2, n2 = 3))
