96ceb11a0c900369ccb8c1a599445905b0081bc8
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-08 14:03:41 +0600

    how to find element in tuple in py

---
+++ tuple_04.py
@@ -0,0 +1,12 @@
+colors = ("Red", "Blue", "Green", "Yellow", "Orange")
+
+if "Green" in colors:
+    print("Green is in tuple list")
+else:
+    print("Green is not in tuple list")
+    
+    
+if "Blue" not in colors:
+    print("Blue is not in tuple list")
+else:
+    print("Blue is in tuple list")
