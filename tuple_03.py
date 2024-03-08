1836f34b75e4d2d58ff8bd5ce4597f6d234c6e79
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-08 13:52:01 +0600

    indexing in tuple in py

---
+++ tuple_03.py
@@ -0,0 +1,11 @@
+colors = ("Red", "Blue", "Green", "Yellow", "Orange")
+
+# How to access tuple index
+
+print(colors[0]) # Positive Indexing
+print(colors[-1]) # Negetive Indexing
+print(colors[1:4]) # Positive Range
+print(colors[-3:-1]) # Positively Negetive Indexing
+print(colors[:0:-1]) # Negetively Negetive Indexing
+print(colors[::-1]) # Reverse Indexing
+
