bcbbef35416b52b6b362052fdd10f76f496ab132
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-08 15:32:16 +0600

    concatinate tuples in py

---
+++ tuple_06.py
@@ -0,0 +1,14 @@
+colors = ("Red", "Blue", "Green", "Yellow", "Orange")
+
+more_colors = ("White", "Blacke")
+
+
+result = colors + more_colors
+
+print(result)
+
+more_colors_2 = ("Purple",)
+
+result_2 = result + more_colors_2
+
+print(result_2)
