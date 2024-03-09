3cd8b775b84fdbcbd7fc98c15ae612f471cd0790
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-09 22:35:20 +0600

    update and remove elements in set in py

---
+++ set_04.py
@@ -0,0 +1,15 @@
+subjects = {'bangla', 'english', 'bangla', 'math'}
+
+print(subjects)
+
+# Add subjects
+new_subjects = ["Chemistry", "Biology"]
+
+subjects.update(new_subjects)
+
+print(subjects)
+
+# Remove Items
+subjects.remove("bangla")
+
+print(subjects)
