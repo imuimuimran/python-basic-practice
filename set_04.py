26171f1d01e5d626a16a079b4d7f290eea184f13
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-10 13:45:18 +0600

    update, remove and discard elements in set in py

--- set_04.py
+++ set_04.py
@@ -11,5 +11,17 @@ print(subjects)
 
 # Remove Items
 subjects.remove("bangla")
-
 print(subjects)
+
+# In remove() function if the mentioned item is not available, it show an error
+# Instead of using femove() function, if we use discard function(), it does not show any error
+
+sub = "Biology"
+if sub in subjects:
+    print(sub, "is in subjects list and after discard", sub, "the updated subjects are: ")
+    subjects.discard(sub)
+    print(subjects)
+    
+else:
+    print(sub, "is not in subjects list. The subjects are mension below:")
+    print(subjects)
