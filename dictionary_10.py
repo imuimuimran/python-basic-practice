8eea413befa6eb4b0412aad8659ab66e32f3dbea
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-11 00:02:01 +0600

    how to sum dictionary values in py

---
+++ dictionary_10.py
@@ -0,0 +1,14 @@
+nums = {
+    "a" : 100,
+    "b" : 200,
+    "c" : 300    
+}
+
+print(nums)
+
+print(sum(nums.values()))
+
+# we can sum the numbers another way
+total_sum = nums["a"] + nums["b"] + nums["c"]
+
+print(total_sum)
