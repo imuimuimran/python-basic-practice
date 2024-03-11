c62278256ecb1ebacd80e34b3bf1060285fe3411
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-11 22:01:34 +0600

    basic of string in py

---
+++ string_02.py
@@ -0,0 +1,21 @@
+name1 = "Tahsan Akram"
+name2 = "Ruzaina Sabera"
+name3 = "Rafin Raiyan Isham"
+
+print(name1)
+print(name2)
+print(name3)
+print(name1, name2, name3)
+
+print(name1[2])
+print(name2[2:6])
+print(name1[1:])
+print(name1[:-1])
+print(name1[::-1])
+
+print(type(name1))
+
+paragraph = '''Hi, my name is ISham
+And I am not student.'''
+
+print(paragraph)
