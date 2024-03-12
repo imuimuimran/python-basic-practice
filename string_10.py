763117622d0aac2e727b60b2afe1ed712d32e963
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-12 17:21:51 +0600

    split in string in py

---
+++ string_10.py
@@ -0,0 +1,13 @@
+str1 = "Hello! world! how! are! you!"
+
+print(str1.split())
+
+list1 = str1.split()
+
+print(list1)
+
+list2 = str1.split("!", 2)
+print(list2)
+
+list3 = str1.split("!", 2)
+print(list3)
