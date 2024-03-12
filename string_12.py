b59961e07cfd5980a911f061f286ad51b34f130a
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-12 17:41:40 +0600

    string format in py

---
+++ string_12.py
@@ -0,0 +1,14 @@
+stu_cl = "VI"
+stu_n1 = "Abir Hasan"
+stu_m1 = 98
+
+stu_c2 = "IX"
+stu_n2 = "Mukul Bose"
+stu_m2 = 95
+
+str1 = "My name is {n}. I read in class {c} and my exam marks is: {m}".format(n = stu_n1, c = stu_cl, m = stu_m1)
+str2 = "My name is {n}. I read in class {c} and my exam marks is: {m}".format(n = stu_n2, c = stu_c2, m = stu_m2)
+
+print(str1)
+print(str2)
+
