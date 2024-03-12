2f5570b3e3ae25d443f4637f5fddaf30e818aaa4
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-12 14:58:40 +0600

    basic of slicing string in py

---
+++ string_06.py
@@ -0,0 +1,22 @@
+name1 = "Hello World!"
+
+# slicing from strating (inclusive) to to ending (exclusive) index
+print(name1[2:9])
+
+print(name1[0:4])
+# if we slice from first index like above we can write another way
+print(name1[:4])
+
+# in the same way we can slice inputed index to the last
+print(name1[6:])
+
+# negetive indexing
+
+# slice from first index to inputted index negetively
+print(name1[:-7])
+# slicing from strating (inclusive) to to ending (exclusive) index negetively
+print(name1[-10:-3])
+
+# slice inputed index to the last negetively
+print(name1[-6:])
+
