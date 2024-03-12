673e6c35cdbb65edb5d790b53d3339eab64cc4b2
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-12 15:08:29 +0600

    upper, lower and capitalize in string in py

---
+++ string_07.py
@@ -0,0 +1,17 @@
+str1 = "Bangladesh is a country"
+
+print(str1)
+
+str2  = str1.upper()
+
+print(str1)
+print(str2)
+
+str3 = str2.lower()
+
+print(str2)
+print(str3)
+
+str4 = str3.capitalize()
+
+print(str4)
