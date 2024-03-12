5163c223669a2731ed0ca45a933caf8174c865e2
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-03-12 12:20:35 +0600

    basic find function in string in py

---
+++ string_05.py
@@ -0,0 +1,13 @@
+name1 = "Hello World!"
+
+# print first occurence index value
+print(name1.find('o'))
+
+# print -1 if the charecter is not present in string
+print(name1.find('a'))
+
+# print first occurence index value of a substring
+print(name1.find('ld'))
+
+# also can find space
+print(name1.find(' '))
