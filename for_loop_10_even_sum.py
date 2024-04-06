d0bc8e3c6fb0bcd45d527338add1e479e9a84bad
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-04-06 11:15:13 +0600

    print even and odd sum in for loop

--- for_loop_10_even_sum.py
+++ for_loop_10_even_sum.py
@@ -1,7 +1,13 @@
+odd_sum = 0
 even_sum = 0
 
 for i in range(1,101):
-    if i % 2 == 0:
+    if i % 2 == 1: # This line extract only odd numbers in range
+        odd_sum += i
+    if i % 2 == 0: # # This line extract only even numbers in range
         even_sum += i
         
+print(odd_sum)
 print(even_sum)
+
+
